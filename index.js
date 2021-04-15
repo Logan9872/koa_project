9e7d22b75c5a7d5e8708ddaa232b406d02d42039
Logan <luchangda_njfu@163.com>
2021-04-15 10:47:04 +0800

    中间件

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
-app.use(async (ctx) => {
-    if (ctx.url === '/') {
-        ctx.body = '这是主页'
-    } else if (ctx.url === '/users') {
-        if (ctx.method === 'GET') {
-            ctx.body = '这是用户列表页';
-        } else if (ctx.method === 'POST') {
-            ctx.body = '创建用户';
-        } else {
-            ctx.status = 405;
-        }
+const router = new Router();
+const usersRouter = new Router({
+    prefix: '/users'
+});
+
+const auth = async (ctx, next) => {
+    if (ctx.url === '/users') {
+        ctx.throw(401)
     }
-})
+    await next();
+}
+
+usersRouter.get('/', (ctx) => {
+    ctx.body = '这是主页';
+});
+
+usersRouter.get('/', auth, (ctx) => {
+    ctx.body = '这是用户列表';
+});
+
+usersRouter.post('/', (ctx) => {
+    ctx.body = '这是创建用户';
+});
+
+usersRouter.get('/:id', (ctx) => {
+    ctx.body = `这是用户${ctx.params.id}`
+});
+
+
+app.use(usersRouter.routes())
+app.use(router.routes())
 app.listen(3000);
