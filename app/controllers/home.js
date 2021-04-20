1471c6ab746355222c4a0b68f324569dd5b43595
Logan <luchangda_njfu@163.com>
2021-04-20 20:14:29 +0800

    上传文件

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,12 @@
+const path = require('path')
 class HomeCtl {
-    index() {
+    index(ctx) {
         ctx.body = '这是主页';
     }
     upload(ctx) {
         const file = ctx.request.files.file;
-        ctx.body = { path: file.path };
+        const basename = path.basename(file.path);
+        ctx.body = { url: `${ctx.origin}/uploads/${base}` };
     }
 }
 
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const koaStatic = require('koa-static');
 const error = require('koa-json-error');
 const parameter = require('koa-parameter');
 const mongoose = require('mongoose');
@@ -8,12 +9,14 @@ const app = new Koa();
 const routing = require('./routes');
 const { connectionStr } = require('./config');
 
+mongoose.connect(connectionStr, { useUnifiedTopology: true }, () => console.log('MongoDB链接成功！'));
+mongoose.connection.on('error', console.error);
+
+app.use(koaStatic(path.join(__dirname, 'public')));
+
 app.use(error({
     postFormat: (e, { stack, ...rest }) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
 }));
-
-mongoose.connect(connectionStr, { useUnifiedTopology: true }, () => console.log('MongoDB链接成功！'));
-mongoose.connection.on('error', console.error);
 
 app.use(koaBody({
     multipart: true,
